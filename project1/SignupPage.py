07bda662fa830fc76f7d77ff8c1d6d2fc948d711
Angelina <batiavat@yandex.ru>
2025-06-06 14:35:05 +0300

    UI 3.0 auto date and years

--- project1/SignupPage.py
+++ project1/SignupPage.py
@@ -7,6 +7,7 @@ from playwright.sync_api import Page, expect
 fake = Faker()
 Name = fake.name()
 Email = fake.email()
+Month = fake.month_name()
 Password = fake.password(length=8, special_chars=True, digits=True, upper_case=True, lower_case=True)
 class SignupPage(BasePage):
     def enter_name(self):
@@ -41,13 +42,8 @@ class SignupPage(BasePage):
         ]
         assert dropdown.locator("option").all_text_contents() == expected
 
-        # Выбор мая по тексту
         dropdown.select_option(label="May")
         assert dropdown.input_value() == "5"
-
-
-
-
 
     def test_days_dropdown(self):
         dropdown = self.page.locator("select[id='days']")
@@ -55,11 +51,44 @@ class SignupPage(BasePage):
         assert dropdown.is_visible()
 
         expected_options = ["Day"] + [str(i) for i in range(1, 32)]
+        print(expected_options)
         actual_options = dropdown.locator("option").all_text_contents()
         print(actual_options)
 
         assert actual_options == expected_options
 
-        dropdown.select_option(value="4")
-        assert dropdown.input_value() == "4"
+        """Выборка конкретного дня"""
+        # dropdown.select_option(value="4")
+        # assert dropdown.input_value() == "4"
 
+        """Выборка рандомного дня"""
+        random_day_int = fake.random_int(min=1, max=31)
+        random_day_str = str(random_day_int)
+
+        dropdown.select_option(value=random_day_str)
+
+        assert dropdown.input_value() == random_day_str
+
+
+    def test_years_dropdown(self):
+        dropdown = self.page.locator("select[id='years']")
+
+        assert dropdown.is_visible()
+
+        expected_option = ["Year"] + [str(i) for i in range(2021, 1899, -1)]
+        print(expected_option)
+        actual_options = dropdown.locator("option").all_text_contents()
+        print(actual_options)
+
+        assert actual_options == expected_option
+
+        """Выборка конкретной даты"""
+        # dropdown.select_option(value="2020")
+        # assert dropdown.input_value() == "2020"
+
+        """Выборка рандомной даты"""
+        random_year_int = fake.random_int(min=1900, max=2021)
+        random_year_str = str(random_year_int)
+        dropdown.select_option(value=random_year_str)
+
+        assert dropdown.input_value() == random_year_str  # Проверяем value
--- project1/test_search.py
+++ project1/test_search.py
@@ -23,19 +23,6 @@ def test_register_user(page):
     signup_page.enter_password()
     signup_page.test_month_dropdown()
     signup_page.test_days_dropdown()
+    signup_page.test_years_dropdown()
     sleep(5)
 
-    # def test_days_dropdown(self):
-    #     dropdown = self.page.locator("select[id='days']")
-    #
-    #     assert dropdown.is_visible()
-    #
-    #     expected = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
-    #
-    #
-    #     assert dropdown.locator("option") == expected
-    #     dropdown.select_option(label= "4")
-    #     assert dropdown.input_value() == "4"
-    #
-    #     selected = dropdown.locator("option[selected]").text_content()
-    #     assert selected == '4'
